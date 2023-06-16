e2ac8a4f024f5140abe28d46263dc6c53aed0d8a
Kepheous <gbernard177@gmail.com>
2023-06-15 20:13:46 -0700

    very bassic data grabbed from database and put on webpage

--- public/script.js
+++ public/script.js
@@ -3,12 +3,26 @@ let testname = "Johnny";
 let node = document.createTextNode('initalized');
 let row = document.createElement('div');
 
-for (let i = 0; i < 100; i++){
-    testname = "Johnny" + i;
-    node = document.createTextNode(testname);
-    row = document.createElement('div');
-    row.appendChild(node);
-    row.setAttribute('class','row');
-    container.appendChild(row);
+
+document.getElementById("transFriendSubmit").addEventListener("click", testFetch);
+
+async function testFetch() {
+    const response = await fetch('/transactions', {
+        method: "GET"
+    });
+    const jsonData = await response.json();
+    console.log(jsonData);
+
+
+    for (let i = 0; i < jsonData.length; i++){
+        console.log(jsonData[i]);
+        let transaction = jsonData[i];
+
+        node = document.createTextNode(transaction.cost);
+        row = document.createElement('div');
+        row.appendChild(node);
+        row.setAttribute('class','row');
+        container.appendChild(row);
+}
 }
 
--- server.js
+++ server.js
@@ -2,23 +2,56 @@ const express = require('express');
 const app = express();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('db/justbalancetest.db');
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://application:HelloWorld@justbalance.5omg9.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
 
 app.use(express.static(__dirname + '/public'));
+
 
 // routes
 app.get('/', function(request, response){
     console.log("GET request recieved at /");
+    console.log(request);
 });
 
-app.get('/transactions', function(request, response){
-    console.log("GET request recieved at /transactions");
+app.get('/friends', function(request, response){
+    console.log("GET request recieved at /friends");
+    console.log(request);
 });
 
 app.post('/transactions', function(request,response){
     console.log("POST request recieved at /transactions");
+    console.log(request);
+});
+
+app.get('/transactions', async function(request, response){
+    console.log("GET request recieved at /transactions");
+    const transactions = await findTransactionsBetweenUsers(client, "Kepheous", "oneNonlyBennett");
+    response.setHeader('Content-Type', 'application/json');
+    console.log(transactions);
+    response.status(200).send(transactions);
 });
 
 app.listen(3000, function(){
     console.log("Server is running at port 3000");
 });
 
+async function findTransactionsBetweenUsers(client, borrower = "", lender = "") {
+    const cursor = client.db("JustBalance").collection("Transactions").find({
+        borrower_name: borrower,
+        lender_name: lender
+    })
+
+    const transactions = await cursor.toArray();
+
+    if (transactions.length > 0) {
+        console.log(`Found ${transactions.length} transactions between ${borrower} and ${lender}`);
+        console.log(transactions);
+        const transactionsJSON = JSON.stringify(transactions);
+        return transactionsJSON;
+    } else (
+        console.log("Did not find any transactions.")
+    )
+}
