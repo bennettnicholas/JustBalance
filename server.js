dc67d44bb54f2c93adff9f19a28c9191b92a4bae
Kepheous <gbernard177@gmail.com>
2023-07-07 11:38:17 -0700

    Slowly working through login process and verification

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
 const app = express();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('db/justbalancetest.db');
@@ -8,17 +10,79 @@ const client = new MongoClient(process.env.MONGO_URI);
 
 
 app.use(express.static(__dirname + '/public'));
+////////////////////////////////////////////////////////////////////////////////////////////////
+// creating 5 minutes from milliseconds
+const fiveMinutes = 1000 * 60 * 5;
 
+//session middleware
+app.use(sessions({
+    secret: process.env.COOKIE_SECRET,
+    saveUninitialized:true,
+    cookie: { maxAge: fiveMinutes },
+    resave: false
+}));
+
+// parsing the incoming data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//username and password
+const myusername = 'Kepheous'
+const mypassword = 'mypassword'
+
+// a variable to save a session
+let session;
+
+//middleware to test if authenticated
+function isAuthenticated (req, res, next) {
+    if (req.session.user) next()
+    else{
+        res.status(401).sendFile('index.html', {root:__dirname});
+    }
+}
 
 // routes
-app.get('/', function(request, response){
-    console.log("GET request recieved at /");
-    console.log(request);
+app.get('/', isAuthenticated, function(req,res){
+    console.log(session);
+    res.send("Welcome User <a href=\'/logout'>click to logout</a>");
 });
 
+app.post('/user',(req,res) => {
+    if(req.body.username == myusername && req.body.password == mypassword){
+        session=req.session;
+        session.userid=req.body.username;
+        console.log(req.session)
+        res.send(`Hey there, welcome <a href=\'/logout'>click to logout</a>`);
+    }
+    else{
+        res.send('Invalid username or password');
+    }
+})
+
+app.get('/testpage3',function(req,res){
+    console.log("GET recieved at /testpage3");
+    session=req.session;
+    if(session.userid){
+        res.sendFile('testpage3.html',{root:__dirname})
+    }else
+        res.sendFile('index.html',{root:__dirname})
+});
+
+app.get('/transactions',async function(req,res){
+    session=req.session;
+    if(session.userid){
+    console.log("GET request recieved at /transactions");
+    const transactions = await findTransactionsBetweenUsers(client, session.userid, "oneNonlyBennett");
+    res.setHeader('Content-Type', 'application/json');
+    console.log(transactions);
+    res.status(200).send(transactions);
+    }else
+    res.sendFile('index.html',{root:__dirname})
+});
+/////////////////////////////////////////////////////////////////////////////////////////////////
 app.get('/friends', function(request, response){
     console.log("GET request recieved at /friends");
-    console.log(request);
+    console.log(request.body);
 });
 
 app.post('/transactions', function(request,response){
@@ -26,13 +90,13 @@ app.post('/transactions', function(request,response){
     console.log(request);
 });
 
-app.get('/transactions', async function(request, response){
-    console.log("GET request recieved at /transactions");
-    const transactions = await findTransactionsBetweenUsers(client, "Kepheous", "oneNonlyBennett");
-    response.setHeader('Content-Type', 'application/json');
-    console.log(transactions);
-    response.status(200).send(transactions);
-});
+// app.get('/transactions', async function(request, response){
+//     console.log("GET request recieved at /transactions");
+//     const transactions = await findTransactionsBetweenUsers(client, "Kepheous", "oneNonlyBennett");
+//     response.setHeader('Content-Type', 'application/json');
+//     console.log(transactions);
+//     response.status(200).send(transactions);
+// });
 
 app.listen(3000, function(){
     console.log("Server is running at port 3000");
